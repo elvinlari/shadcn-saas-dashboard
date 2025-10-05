d74c02ad586f1cdeb3edd47e71d0329afea4fd6c
Elvin Lari <12527067+elvinlari@users.noreply.github.com>
2025-10-05 16:25:07 +0100

    Feature: scaffold shdcn saas app

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: false, // Automatically opens browser on server start
+  },
+})
